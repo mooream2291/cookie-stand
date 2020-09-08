074d33d093f26ca6925993fc7d082d40fed44d26
Ashley Moore <mooream2291@gmail.com>
2020-09-08 13:57:12 -0400

    edit

--- app.js
+++ app.js
@@ -2,14 +2,6 @@
 
 var storehours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-function generaterandom (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor (Math.random() * (max-min+1)) + min
-
-var storeinfo = document.getElementbyID('sales');
-
-}
 var seattle = {
     name: 'Seattle',
     min: 23,
