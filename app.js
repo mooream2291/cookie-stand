6ffda484fa400c62e8972483ee2f4c342f3fa4e3
Ashley Moore <mooream2291@gmail.com>
2020-09-08 22:28:16 -0400

    table made

--- app.js
+++ app.js
@@ -1,103 +1,133 @@
 'use strict';
 
+//setting predetermined store hours as defined in lab instructions
+
 var storehours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-var seattle = {
-    name: 'Seattle',
-    min: 23,
-    max: 65,
-    avgck: 6.3,
-    totalsales: [],
-   random:function() {
-       for (var i = 0; i < storehours.length; i++);
-       var cph = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-       var cookiesales = Math.round(cph*this.avgck);
-       return cookiesales
-   },  
-   generateTotal:function() {
-       for (var i = 0; i < storehours.length; i++) {
-           this.totalsales.push(this.random())
-       }
-       this.renderStore();
-   },
-    renderStore:function() {
-        var root = document.getElementById('root');
-        var list =  document.createElement ('ul');
-            for (var i = 0; i < this.totalsales.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = `${storehours[i]}: ${this.totalsales[i]}`;
-        list.append(listItem);
-    }
-    root.append(list);
-    }
-}
-seattle.generateTotal();
+var salesbyhour =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-var tokyo = {
-    name: 'Tokyo',
-    min: 3,
-    max: 24,
-    avgck: 1.2,
-    totalsales: [],
-   random:function() {
-       var cph = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-       var cookiesales = Math.round(cph*this.avgck);
-       return cookiesales
-   },  
-   generateTotal:function() {
-       for (var i = 0; i < storehours.length; i++) {
-           this.totalsales.push(this.random())
-       }
-   }
+
+function Store(name, min, max, avgck) {
+    this.name = name;
+    this.min = min;
+    this.max = max;
+    this.avgck = avgck;
+    this.totalsales = [];
 }
-var dubai = {
-    name: 'Dubai',
-    min: 11,
-    max: 38,
-    avgck: 3.7,
-    totalsales: [],
-   random:function() {
-       var cph = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-       var cookiesales = Math.round(cph*this.avgck);
-       return cookiesales
-   },  
-   generateTotal:function() {
-       for (var i = 0; i < storehours.length; i++) {
-           this.totalsales.push(this.random())
-       }
-   }
+Store.prototype.random = function() {
+    var cph = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+    var cookiesales = Math.round(cph*this.avgck);
+    return cookiesales
+} 
+ 
+Store.prototype.generateTotal = function() {
+    for (var i = 0; i < storehours.length; i++) {
+        var myrandom = this.random();
+        this.totalsales.push(myrandom);
+        salesbyhour[i]+= myrandom  
+    } 
 }
-var paris = {
-    name: 'Paris',
-    min: 20,
-    max: 38,
-    avgck: 2.3,
-    totalsales: [],
-   random:function() {
-       var cph = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-       var cookiesales = Math.round(cph*this.avgck);
-       return cookiesales
-   },  
-   generateTotal:function() {
-       for (var i = 0; i < storehours.length; i++) {
-           this.totalsales.push(this.random())
-       }
-   }
+
+// Store.prototype.dailyTotals = function() {
+//     for (var i )
+// }
+Store.prototype.render = function() {
+    
+    var table = document.getElementById('cookietable');
+    var tHead = document.createElement('thead');
+    cookietable.append(tHead);
+
+    var row = document.createElement('tr');
+    tHead.append(row);
+
+    var data = document.createElement('td');
+    row.append(data);
+
+    data.textContent = this.name;
+
+    for (var i =0; i < this.totalsales.length; i++) {
+        data = document.createElement('td');
+        row.append(data);
+        data.textContent = this.totalsales[i]
+    } 
 }
-var lima = {
-    name: 'Lima',
-    min: 2,
-    max: 16,
-    avgck: 4.6,
-    totalsales: [],
-   random:function() {
-       var cph = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-       var cookiesales = Math.round(cph*this.avgck);
-       return cookiesales
-   },  
-   generateTotal:function() {
-       for (var i = 0; i < storehours.length; i++) {
-           this.totalsales.push(this.random())
-       }
-   }
+
+var seattle = new Store('Seattle', 23, 65, 6.3)
+
+var tokyo = new Store('Tokyo', 3, 24, 1.2)
+
+var dubai = new Store('Dubai', 11, 38, 3.7)
+
+var paris = new Store('Paris', 20, 38, 2.3)
+
+var lima = new Store('Lima', 2, 16, 4.6)
+
+// function generateTableHead() {
+//     var cookietable = document.getElementById('cookietable');
+//     var row = document.createElement('thead');
+//     cookietable.append(row);
+
+//     var row = document.createElement('tr');
+
+//     var row = document.createElement('td');
+//     var row = document.createElement('td');
+//     var row = document.createElement('td');
+//     var row = document.createElement('td');
+//     var row = document.createElement('td');
+//     var row = document.createElement('td');
+
+//     //make table body function
+
+//     var row = document.createElement('tbody');
+//     var row = document.createElement('td');
+//     var row = document.createElement('td');
+
+
+// }
+// generateTableHead();
+
+function generateTableHead() {
+    var table = document.getElementById('cookietable');
+    var tHead = document.createElement('thead');
+    cookietable.append(tHead);
+
+    var row = document.createElement('tr');
+    tHead.append(row);
+
+    var data = document.createElement('td');
+    row.append(data);
+
+    data.textContent = '';
+
+    for (var i =0; i < storehours.length; i++) {
+        data = document.createElement('td');
+        row.append(data);
+        data.textContent = storehours[i]
+    }  
+}
+
+function mainTable() {
+
+    generateTableHead();
+
+    seattle.generateTotal();
+    seattle.render();
+
+    tokyo.generateTotal();
+    tokyo.render();
+
+    dubai.generateTotal();
+    dubai.render();
+
+    paris.generateTotal();
+    paris.render();
+
+    lima.generateTotal();
+    lima.render();
+}
+mainTable();
+console.log(salesbyhour); 
+function footer() {
+    
+
 }
